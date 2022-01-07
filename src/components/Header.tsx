1cbe10459448a3bc598c19cea2fbc143a2b25fb3
Hong-Tan <hongtan1422002@sgmail.com>
2022-01-07 10:02:05 +0700

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,42 +27,43 @@ function Header() {
              
           </li>
 
-          <li className="mt-[1.7em] ml-[25rem]">
-             <Link to="/"  className={" hover:text-[#12B0DF] hover:underline-offset-4 hover:underline font-[797] text-[1.4rem] cursor-pointer "
+          <li className="mt-[1.9em] ml-[25rem]">
+             <Link to="/"  className={" hover:text-[#12B0DF] hover:underline-offset-4 hover:underline font-[797] font-inter font-[700] text-xl cursor-pointer "
              +(path == "/" ? " text-[#12B0DF] underline-offset-4 underline" : "")  } 
              onClick={() => setPath('/')}
               >Trang Chủ </Link> 
           </li>
         
-          <li className="mt-[1.7em] ml-[4rem]">
-            <Link to="/about" className={"hover:text-[#12B0DF] hover:underline-offset-4 hover:underline font-[797] text-[1.4rem] cursor-pointer "
+          <li className="mt-[1.9em] ml-[4rem]">
+            <Link to="/about" className={"hover:text-[#12B0DF] hover:underline-offset-4 hover:underline font-[797] font-[700] text-xl cursor-pointer "
              +(path == "/about" ? " text-[#12B0DF] underline-offset-4 underline" : "")  }  onClick={() => setPath('/about')}>About </Link> 
           </li>
 
-          <li className="mt-[1.7em] ml-[4rem] ">
-            <Link to="/thiThu" className={"hover:text-[#12B0DF] hover:underline-offset-4 hover:underline font-[797] text-[1.4rem] cursor-pointer"
+          <li className="mt-[1.9em] ml-[4rem] ">
+            <Link to="/thiThu" className={"hover:text-[#12B0DF] hover:underline-offset-4 hover:underline font-[797] font-[700] text-xl cursor-pointer"
              +(path == "/thiThu" ? " text-[#12B0DF] underline-offset-4 underline" : "")  }  onClick={() => setPath('/thiThu')}>Thi Thử </Link> 
           </li>
-          <li className="mt-[1.7em] ml-[4rem]">
-            <Link to="/taiLieu" className={"hover:text-[#12B0DF] hover:underline-offset-4 hover:underline font-[797] text-[1.4rem] cursor-pointer "
+          <li className="mt-[1.9em] ml-[4rem]">
+            <Link to="/taiLieu" className={"hover:text-[#12B0DF] hover:underline-offset-4 hover:underline font-[797] font-[700] text-xl cursor-pointer "
              +(path == "/taiLieu" ? " text-[#12B0DF] underline-offset-4 underline" : "")  }  onClick={() => setPath('/taiLieu')}>Tài Liệu</Link> 
           </li>
           
-          <li className="mt-[2em] ml-[4em] ">
+          <li className="mt-[2.1em] ml-[4em] ">
              <Link to="/admin" className=" cursor-pointer text-[#0b6a86] font-[800]  bg-[#c4ebf7] p-2 rounded-bl-3xl rounded-tr-3xl px-[55px] py-[13px] hover:bg-[#12B0DF] hover:text-white" onClick={() => setPath('/admin')}>Admin </Link>
               
           </li>
           <li className="flex ">
-          <a className="cursor-pointer mt-[2em] ml-[2em]"   href="https://www.linkedin.com/" target="_blank" >
-              <IconLinkedin/>
-          </a>
-          <a className="cursor-pointer mt-[2em]  ml-[2em]" href="https://github.com/FunDev-Team" target="_blank">
-          <IconGithub/>
-          </a>
+              <a className="cursor-pointer mt-[2.1em] ml-[2em]"   href="https://www.linkedin.com/" target="_blank" >
+                  <IconLinkedin/>
+              </a>
+              <a className="cursor-pointer mt-[2.1em]  ml-[2em]" href="https://github.com/FunDev-Team" target="_blank">
+              <IconGithub/>
+              </a>
           </li>
          
         </ul>
     </div>
+   
 
   );
 }
